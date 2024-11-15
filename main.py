8049ae9419db5cc8877ff2d02fe58a06b22fb62a
노광우 <ngw960@gmail.com>
2024-11-15 17:28:00 +0900

    .

--- main.py
+++ main.py
@@ -5,21 +5,23 @@ from starlette.requests import Request
 
 app = FastAPI()
 
-# 정적 파일 경로 설정
+# 정적 파일 경로 설정: static 폴더를 /static 경로로 서빙
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # 템플릿 디렉터리 설정
 templates = Jinja2Templates(directory="templates")
 
+# 루트 경로
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# 서비스 워커 및 매니페스트 경로 설정
+# 서비스 워커 서빙
 @app.get("/service-worker.js")
 async def service_worker():
     return StaticFiles(directory="static").__call__(scope=None, receive=None, send=None)
 
+# 매니페스트 서빙
 @app.get("/manifest.json")
 async def manifest():
     return StaticFiles(directory="static").__call__(scope=None, receive=None, send=None)
